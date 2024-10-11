e77b3455e7ce4a4ad500b8f81360617b7e06d80a
JavierPinto <javier.pinto.becerra@gmail.com>
2024-10-11 15:48:25 -0500

    clase cliente

--- cliente.js
+++ cliente.js
@@ -1,8 +1,21 @@
+
+//Clase cliente para el simulador
 class Cliente {
     constructor(nombre, eddad, compras, telefono) {
         this.nombre = nombre
         this.eddad = eddad
-        this.compras = compras
+        this.compras = compras  // Este es un array con los montos de compras realizadas
         this.telefono = telefono
     }
+}
+
+// Método para calcular el total de compras realizadas
+totalCompras(){
+    return this.compras.reduce((total, compra) => total + compra, 0); 
+}
+
+// Método para mostrar la información del cliente
+
+mostrarInfo(){
+    console.log(`Nombre: ${this.nombre} \nEdad: ${this.eddad} \nTeléfono: ${this.telefono} \nTotal de compras: ${this.totalCompras()}`)
 }
