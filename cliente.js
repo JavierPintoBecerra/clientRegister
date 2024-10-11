53768f82e716531dc6801d77dd7c83d95bc352b1
JavierPinto <javier.pinto.becerra@gmail.com>
2024-10-11 15:34:04 -0500

    Cliente app

---
+++ cliente.js
@@ -0,0 +1,8 @@
+class Cliente {
+    constructor(nombre, eddad, compras, telefono) {
+        this.nombre = nombre
+        this.eddad = eddad
+        this.compras = compras
+        this.telefono = telefono
+    }
+}
