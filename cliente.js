6ca15704a760e2d045fb33329b50e960daea653a
JavierPinto <javier.pinto.becerra@gmail.com>
2024-10-11 16:26:53 -0500

    Registro clientes

--- cliente.js
+++ cliente.js
@@ -7,15 +7,56 @@ class Cliente {
         this.compras = compras  // Este es un array con los montos de compras realizadas
         this.telefono = telefono
     }
+
+    // Método para calcular el total de compras realizadas
+    totalCompras(){
+        return this.compras.reduce((total, compra) => total + compra, 0); 
+    }
+
+    // Método para mostrar la información del cliente
+
+    mostrarInfo(){
+        console.log(`Nombre: ${this.nombre} \nEdad: ${this.eddad} \nTeléfono: ${this.telefono} \nTotal de compras: ${this.totalCompras()}`)
+    }
+
 }
 
-// Método para calcular el total de compras realizadas
-totalCompras(){
-    return this.compras.reduce((total, compra) => total + compra, 0); 
-}
+// Clase para el registro de clientes
 
-// Método para mostrar la información del cliente
+class RegistroClientes{
+    constructor(){
+        this.clientes = []
+    }
 
-mostrarInfo(){
-    console.log(`Nombre: ${this.nombre} \nEdad: ${this.eddad} \nTeléfono: ${this.telefono} \nTotal de compras: ${this.totalCompras()}`)
+
+    // Método para registrar un nuevo cliente
+    agregar(cliente){
+        this.clientes.push(cliente)
+    }
+
+    // Método para buscar por nombre al cliente
+    buscarPorNombre(nombre){
+        return this.clientes.filter(cliente => cliente.nombre.toLowerCase() === nombre.toLowerCase());
+    }
+
+    // Método para buscar clientes por edad
+    buscarPorEdad(edad){
+        return this.clientes.filter(cliente => cliente.edad === edad);
+    }
+
+    //Método para categorizar clientes según sus compras
+
+    categorizarClientes(){
+        this.clientes.forEach(cliente => {
+            const totalCompras = cliente.totalCompras();
+            if (totalCompras > 1000) {
+                console.log(`El cliente ${cliente.nombre} es un cliente VIP`); 
+        } else if (totalCompras > 500) {
+            console.log(`El cliente ${cliente.nombre} es un cliente Gold`);
+        } else {
+            console.log(`El cliente ${cliente.nombre} es un cliente nuevo o de bajo consumo`);
+        }
+    });
+
+    }
 }
